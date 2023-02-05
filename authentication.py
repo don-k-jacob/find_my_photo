6990ec86e788f46af2c12b1dcc310a332dce3d91
don-k-jacob <donkjacob.business@gmail.com>
2023-02-05 13:43:14 +0530

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -5,3 +5,4 @@ def authenticate():
     creds = Credentials.from_authorized_user_info(info=secrets)
     service = googleapiclient.discovery.build("drive", "v3", credentials=creds)
     return service
+    
